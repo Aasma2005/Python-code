2c969b98179f60fd3e1beb455f8efc9a7e23ba67
Aasma2005 <aasmashaikh48237@gmail.com>
2026-08-27 01:55:13 -0700

    Add files via upload

---
+++ Sets/pop.py
@@ -0,0 +1,6 @@
+s={10,20,30,40,50}
+s.pop() # anyone random element delete in set
+
+print(s.pop()) #this statemet print which element is delete
+print(s)
+
---
+++ Sets/prg.py
@@ -0,0 +1,7 @@
+s={10,20,30,40,50,10}
+print(s)
+
+#covert list into set
+l=[10,20,30]
+s=set(l)
+print(s)
